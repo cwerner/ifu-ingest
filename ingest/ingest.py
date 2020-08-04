1ea4da1f807c126438791b283366c258eb19b95e
Christian Werner <chr.werner@me.com>
2020-08-04 14:22:31 +0200

    some formatting fixes

--- ingest/ingest.py
+++ ingest/ingest.py
@@ -1,15 +1,17 @@
-import xarray as xr
 import pint_xarray
-from ruamel.yaml import YAML 
+import xarray as xr
 from loguru import logger
+from ruamel.yaml import YAML
 
-logger.add("ingest.log", rotation="1 week")    # Once the file is too old, it's rotated
+logger.add("ingest.log", rotation="1 week")  # Once the file is too old rotate
+
 
 class Ingestor:
     model = None
+
     def __init__(self, path: str):
         self._path = path
-        self._data = None
+        self._data = xr.Dataset()
         self.writer = None
         logger.debug(f"{type(self).__name__} instance created")
 
@@ -20,16 +22,17 @@ class Ingestor:
     def read(self, manual: str) -> None:
         raise NotImplementedError
 
-    def write(self, outpath: str = 'test.nc') -> None:
+    def write(self, outpath: str = "test.nc") -> None:
         if not self.writer:
             self.writer = Writer(outpath)
 
-        if self._data:
+        if len(self._data.data_vars) > 0:
             self.writer.write()
 
 
 class LPJIngestor(Ingestor):
     model = "LPJ"
+
     def __init__(self, path: str):
         super().__init__(path)
 
@@ -40,18 +43,19 @@ class LPJIngestor(Ingestor):
 
 class DNDCIngestor(Ingestor):
     model = "DNDC"
+
     def __init__(self, path: str):
         super().__init__(path)
-    
+
     def read(self, manual: str) -> None:
         logger.info(f"Parsing {self.model} using '{manual}'")
-        logger.info(f"Writing to {self.writer.destination if self.writer else '<NOTSET>'}")
+        logger.info(f"Write to {self.writer.destination if self.writer else '<UNSET>'}")
 
 
 class Writer:
     def __init__(self, outpath: str):
         self._path = outpath
-    
+
     @property
     def destination(self):
         return self._path
@@ -69,4 +73,3 @@ if __name__ == "__main__":
     ingestor.read("dndc_parser.yaml")
 
     ingestor.write()
-
