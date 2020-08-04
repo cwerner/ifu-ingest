f6db9f436cba4416a41db7224059fa0621c1d851
Christian Werner <chr.werner@me.com>
2020-08-04 12:01:13 +0200

    add some trial logging and cleanup some ingestor class inheritance

--- ingest/ingest.py
+++ ingest/ingest.py
@@ -3,44 +3,49 @@ import pint_xarray
 from ruamel.yaml import YAML 
 from loguru import logger
 
+logger.add("ingest.log", rotation="1 week")    # Once the file is too old, it's rotated
 
 class Ingestor:
+    model = None
     def __init__(self, path: str):
         self._path = path
         self._data = None
         self.writer = None
+        logger.debug(f"{type(self).__name__} instance created")
 
-    def read(self, manual: str):
+    @property
+    def model(cls):
+        return cls.model
+
+    def read(self, manual: str) -> None:
         raise NotImplementedError
 
-    def write(self) -> None:
+    def write(self, outpath: str = 'test.nc') -> None:
         if not self.writer:
-            outpath = 'test.nc'
             self.writer = Writer(outpath)
 
         if self._data:
             self.writer.write()
 
 
-
 class LPJIngestor(Ingestor):
+    model = "LPJ"
     def __init__(self, path: str):
         super().__init__(path)
-    
+
     def read(self, manual: str) -> None:
-        print("parsing LPJ output")
-        print(f"using lpj manual {manual}")
+        logger.info("parsing LPJ output")
+        logger.info(f"using lpj manual {manual}")
 
 
 class DNDCIngestor(Ingestor):
+    model = "DNDC"
     def __init__(self, path: str):
         super().__init__(path)
     
     def read(self, manual: str) -> None:
-        print("parsing DNDC output")
-        print(f"using dndc manual {manual}")
-        print(f"writing to {self.writer.destination if self.writer else '<NOTSET>'}")
-
+        logger.info(f"Parsing {self.model} using '{manual}'")
+        logger.info(f"Writing to {self.writer.destination if self.writer else '<NOTSET>'}")
 
 
 class Writer:
@@ -57,11 +62,11 @@ class Writer:
 
 if __name__ == "__main__":
     yaml = YAML()
-    
-    logger.debug("Test logging (with emojis): 🤷‍♂️")
+
+    logger.info("Starting up ... 🏎")
 
     ingestor = DNDCIngestor(".")
-    ingestor.read("parser.txt")
+    ingestor.read("dndc_parser.yaml")
 
     ingestor.write()
 
